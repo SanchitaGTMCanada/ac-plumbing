3e47367857982497f87618208534cc2372ff198c
SanchitaGTMCanada <sanchita@gtmcanada.com>
2026-08-03 21:34:27 +0530

    error fixed

--- src/components/home/Services/InfiniteMarquee.jsx
+++ src/components/home/Services/InfiniteMarquee.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import ServiceCard from "./ServiceCard";
+import ServiceCard from "./ServiceCard.jsx";
 
 export default function InfiniteMarquee({ services }) {
   const trackRef = useRef(null);
