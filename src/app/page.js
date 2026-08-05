54d45f8cbfb99e1e43f01afb384ac7fe4c515cc1
SanchitaGTMCanada <sanchita@gtmcanada.com>
2026-08-05 21:51:22 +0530

    mailing system done

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import About from "@/components/home/About/About.jsx";
 import Services from "@/components/home/Services/Services";
 import WhyChoose from "@/components/home/WhyChoose/WhyChoose";
 import Footer from "@/components/layout/Footer/Footer.jsx";
+import Booking from "@/components/booking/Booking";
 
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
            <Services />
             <About />
            <WhyChoose/>
+           <Booking/>
            <Footer />
 
     </>
